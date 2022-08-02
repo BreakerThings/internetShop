abf5c6ef9a43024ebaf542c324aacd37383f02e7
BreakerThings <110393828+BreakerThings@users.noreply.github.com>
2022-08-02 15:02:17 +0300

    Add files via upload

---
+++ Макет-3(Js-бар).js
@@ -0,0 +1,6 @@
+const x = document.getElementById('x');
+const y = document.getElementById('y');
+const itemOne = document.querySelectorAll('.item-1');
+const itemTwo = document.querySelectorAll('.item-2');
+
+
